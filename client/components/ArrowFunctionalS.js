609a1c174510aeba1efac413d0fe17abe7ab074a
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-04-27 09:48:30 +0200

    add arrow functions to the mern project

---
+++ client/components/ArrowFunctionalS.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const ArrowFunctionalS = (props, context) => (
+  <div>
+    
+  </div>
+);
+
+ArrowFunctionalS.propTypes = {
+};
+
+export default ArrowFunctionalS;
---
+++ components/ArrowFunctionalM/ArrowFunctionalM.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const ArrowFunctionalM = (props, context) => (
+  <div>
+    
+  </div>
+);
+
+ArrowFunctionalM.propTypes = {
+};
+
+export default ArrowFunctionalM;
